50dd5a6cda92e2d21e4df89c1e3cba838f50f5ad
iran <iranilsonhonorato88@gmail.com>
2025-09-10 17:56:38 -0300

    feat(search): Implementação completa do algoritmo UCS

--- heuristics.py
+++ heuristics.py
@@ -2,7 +2,6 @@ import pandas as pd
 import matplotlib.pyplot as plt 
 import numpy as np
 from collections import defaultdict
-from aigyminsper.search.search_algorithms import AEstrela
 
 df = pd.read_csv("../Dataset/MapHeuristics.csv", header=None, names=["Start", "Meta", "Custo"])
 
@@ -18,3 +17,23 @@ heuristica = dict(heuristica)
 
 for k, v in heuristica.items():
     print(f"'{k}':{v},")
+
+
+mapa = {
+    'a':[(3,'b'),(6,'c')],
+    'b':[(3,'a'),(3,'h'),(3,'k')],
+    'c':[(6,'a'),(2,'g'),(3,'d'),(2,'o'),(2,'p')],
+    'd':[(3,'c'),(1,'f'),(1,'e')],
+    'e':[(2,'i'),(1,'f'),(1,'d'),(14,'m')],
+    'f':[(1,'g'),(1,'e'),(1,'d')],
+    'g':[(2,'c'),(1,'f'),(2,'h')],
+    'h':[(2,'i'),(2,'g'),(3,'b'),(4,'k')],
+    'i':[(2,'e'),(2,'h')],
+    'l':[(1,'k')],
+    'k':[(1,'l'),(3,'n'),(4,'h'),(3,'b')],
+    'm':[(2,'n'),(1,'x'),(14,'e')],
+    'n':[(2,'m'),(3,'k')],
+    'o':[(2,'c')],
+    'p':[(2,'c')],
+    'x':[(1,'m')]
+    }
